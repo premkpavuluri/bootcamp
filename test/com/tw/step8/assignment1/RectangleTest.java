96c1c54e1312a38ea7d15ea6f05948f98fc250eb
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-09-14 11:24:21 +0530

    Implemented perimeter of rectangle

--- test/com/tw/step8/assignment1/RectangleTest.java
+++ test/com/tw/step8/assignment1/RectangleTest.java
@@ -6,11 +6,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
   @Test
-  void area() {
+  void calculateAreaOfRectangleOfEqualSides() {
     Rectangle rectangle = new Rectangle(1,1);
 
     double actual = rectangle.area();
     double expected = 1;
+
+    assertEquals(expected,actual);
+  }
+
+  @Test
+  void calculateAreaOfRectangleOfDifferentSides() {
+    Rectangle rectangle = new Rectangle(2,3);
+
+    double actual = rectangle.area();
+    double expected = 6;
+
+    assertEquals(expected,actual);
+  }
+
+  @Test
+  void calculatePerimeterOfRectangleOfEqualSides() {
+    Rectangle rectangle = new Rectangle(1, 1);
+    double actual = rectangle.perimeter();
+    double expected = 4;
+
+    assertEquals(expected,actual);
+  }
+
+  @Test
+  void calculatePerimeterOfRectangleOfDifferentSides() {
+    Rectangle rectangle = new Rectangle(2,3);
+    double actual = rectangle.perimeter();
+    double expected = 10;
 
     assertEquals(expected,actual);
   }
